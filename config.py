9515860dd8e5bd03468474d1891c26533c8c7724
unknown <marcosmg2096@gmail.com>
2021-08-05 15:54:37 +0200

    clave

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 23 16:57:39 2021
+
+@author: Marcos
+"""
+import os
+
+TOKEN = '1706853965:AAGsOhWCnr6W1TVLQpRGQd1L4CNBj1nfC1k'
+os.environ["TOKEN"]="1706853965:AAGsOhWCnr6W1TVLQpRGQd1L4CNBj1nfC1k"
+chat_id=1267931674
